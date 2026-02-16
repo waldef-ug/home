afe937018e966f9e3a4604fa6712c16aaf5d346c
waldef-ug <jeandizier23@gmail.com>
2026-02-16 19:45:30 +0100

    Create pages/index.js file with the complete WALDEF React component

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WALDEF = () => {
+  return (
+    <div>
+      <h1>Welcome to WALDEF!</h1>
+      <p>This is a React component for the WALDEF application.</p>
+    </div>
+  );
+};
+
+export default WALDEF;
